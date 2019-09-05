024ecc70637f74cbc2fce883d37b58f6669e42d3
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-05 11:46:52 +0100

    Adds first task

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "kieran"
+drink = "mint tea"
+
+print("Name: " + name + ", Favourite drink: " + drink)
+
+def input_person()
+    name = input("What's your name?")
+    drink = input("What's your favourite drink?")
+    
