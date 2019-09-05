f9490b4895fe902f432d29b2cfeb10dcf1299cf8
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-05 14:17:50 +0100

    Basic CLI

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-name = "kieran"
-drink = "mint tea"
+import sys
+people = ["Alice", "Bob", "Carol"]
+drinks = ["tea", "coffee", "water"]
 
-print("Name: " + name + ", Favourite drink: " + drink)
 
-def input_person()
-    name = input("What's your name?")
-    drink = input("What's your favourite drink?")
-    
+def get_people():
+    print(people)
+
+def get_drinks():
+    print(drinks)
+
+def reject_input():
+    print("Unexpected command, try get-people or get-drinks")
+
+
+if sys.argv[1]:
+    mode = sys.argv[1]
+else:
+    reject_input()
+    exit()
+
+if mode == "get-people":
+    get_people()
+elif mode == "get-drinks":
+    get_drinks()
+else:
+    reject_input()
