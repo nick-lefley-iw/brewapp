84cd6ad8387293a0145f1fb3610066ddfef9f49d
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-17 10:47:12 +0100

    Changes equivalence for drinks

--- code/Drink.py
+++ code/Drink.py
@@ -6,6 +6,13 @@ class Drink:
     def __repr__(self):
         return self.get_name()
 
+    def __eq__(self, other):
+        if not isinstance(other, Drink):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self._name == other._name and self._temperature == other._temperature
+
     def get_name(self):
         return self._name
 
