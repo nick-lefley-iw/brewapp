8bf2e54a39d186a74b32656fe2705534e4f8318e
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-17 10:48:18 +0100

    Delete Order.py

--- code/Order.py
+++
@@ -1,7 +0,0 @@
-class Order:
-    def __init__(self, person, drink):
-        self._person = person
-        self._drink = drink
-
-    def __repr__(self):
-        return (self._person, self._drink)
