6a9f13661d84f1a4c76b92a155227ced0fbb498b
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-17 11:56:44 +0100

    Create testDrink.py

---
+++ test/testDrink.py
@@ -0,0 +1,24 @@
+import unittest
+
+import sys
+
+sys.path.insert(1, '../code/')
+from Drink import Drink
+
+
+class TestDrink(unittest.TestCase):
+    test_drink_cold = Drink("Water","Cold")
+    test_drink_hot = Drink("Tea","Hot")
+    test_second_drink_cold = Drink("Water","Cold")
+
+    def test_is_hot(self):
+        self.assertTrue(self.test_drink_hot.is_hot())
+        self.assertFalse(self.test_drink_cold.is_hot())
+
+    def test_equivalence(self):
+        self.assertEqual(self.test_drink_cold, self.test_second_drink_cold)
+        self.assertEqual(self.test_drink_hot, self.test_drink_hot)
+
+
+if __name__ == '__main__':
+    unittest.main()
