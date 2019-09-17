360cde56c7fada0bd4b7eb87faa3c70f6dd619bf
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-17 12:15:26 +0100

    Add input operations

--- code/inputHandler.py
+++ code/inputHandler.py
@@ -1,6 +1,24 @@
+def get_input(request):
+    return input(request).strip()
+
 def get_input_as_string(user_input):
     try:
         user_input = str(user_input)
     except:
         print("Please enter a valid string.")
         return
+
+
+def get_input_as_integer(user_input):
+    try:
+        user_input = int(user_input)
+    except:
+        print("Please enter a valid integer.")
+        return
+
+
+def get_input_as_list(user_input, separator: str):
+    try:
+        user_input = user_input.split(separator)
+    except:
+        print("Please enter a valid list.")
