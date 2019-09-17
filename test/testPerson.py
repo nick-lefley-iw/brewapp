e275d750c551e8fb4869082319ed23883e09ae24
Kieran Hall <kieran.hall@infinityworks.com>
2019-09-17 11:21:37 +0100

    Adds testPerson

---
+++ test/testPerson.py
@@ -0,0 +1,28 @@
+import unittest
+
+import sys
+
+sys.path.insert(1, '../code/')
+from Drink import Drink
+from Person import Person
+
+
+class TestPerson(unittest.TestCase):
+    test_drink = Drink("Tea", "Hot")
+    test_person = Person("John", "Smith", test_drink)
+    expected_full_name = "John Smith"
+    expected_order = [expected_full_name, test_drink]
+
+    def test_get_name(self):
+        self.assertEqual(self.expected_full_name, self.test_person.get_name())
+
+    def test_get_favourite(self):
+        # self.assertIs(self.test_drink, self.test_person.get_favourite())
+        self.assertEqual(self.test_drink, self.test_person.get_favourite())
+
+    def test_get_order_as_list(self):
+        self.assertEqual(self.expected_order, self.test_person.get_order_as_list())
+
+
+if __name__ == '__main__':
+    unittest.main()
